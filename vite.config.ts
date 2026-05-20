30d3931a8127226de80e6933b2c8739ab241e4e3
LGFDX <1484078720@qq.com>
2026-05-20 22:35:38 +0800

    fix: 配置GitHub Pages部署路径

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/xianglian/', 
+  base: '/xianlian/', 
   plugins: [vue()],
   build: {
     chunkSizeWarningLimit: 1600
